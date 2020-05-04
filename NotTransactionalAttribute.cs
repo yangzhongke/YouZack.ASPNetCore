5457d068ea9a3851431b115bdc6ff9e0c3c71e2a
yangzhongke <yzk365@qq.com>
2020-05-04 15:04:38 +0800

    init

---
+++ NotTransactionalAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YouZack.ASPNetCore
+{
+    /// <summary>
+    /// 被修饰的方法不自动启用TransactionScopeFilter
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class NotTransactionalAttribute:Attribute
+    {
+    }
+}
---
+++ TransactionScopeFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Linq;
+
+namespace YouZack.ASPNetCore
+{
+    /// <summary>
+    /// 自动对于Action启用TransactionScope事务，除非方法上标注NotTransactionalAttribute
+    /// </summary>
+    public class TransactionScopeFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            //Action上是否有标注NotTransactionalAttribute
+            bool hasNotTransactionalAttribute = false;
+            if (context.ActionDescriptor is ControllerActionDescriptor)
+            {
+                ControllerActionDescriptor actionDesc = (ControllerActionDescriptor)context.ActionDescriptor;
+                if(actionDesc.MethodInfo.GetCustomAttributes(typeof(NotTransactionalAttribute), false).Any())
+                {
+                    hasNotTransactionalAttribute = true;
+                }
+            }
+            //如果标注了NotTransactionalAttribute，则不自动启用TransactionScope
+            if (hasNotTransactionalAttribute)
+            {
+                await next();
+            }
+            else
+            {
+                using (var txScope =
+                    new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    await next();
+                    txScope.Complete();
+                }
+            }
+        }
+    }
+}
